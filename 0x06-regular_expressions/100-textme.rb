56d6225f7cc9c66de05da42bb600917183bdc300
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-01-31 20:24:30 +0000

    Text me now

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,6 +1,2 @@
 #!/usr/bin/env ruby
-log = ARGV[0]
-sender = log[/from:(.*?)\]/, 1]
-receiver = log[/to:(.*?)\]/, 1]
-flags = log[/flags:(.*?)\]/, 1]
-puts "#{sender}, #{receiver}, #{flags}"
+puts ARGV[0].scan(/(?<=from:|to:|flags:).+?(?=\])/).join(',')
