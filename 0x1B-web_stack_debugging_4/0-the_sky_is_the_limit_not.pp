4f1879b8b290c49ab310d62b736bde6984faab89
Micah Ondiwa <micah.oketch.ondiwa@gmail.com>
2023-09-13 13:10:35 +0300

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,5 @@
 # Fixing the problem of high amount of requests
-
-exec {'replace':
-  provider => shell,
-  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
-  before   => Exec['restart'],
+exec { 'change-max-open-files':
+  command => 'sudo sed -i "s/15/5000/" /etc/default/nginx; service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
-
-exec {'restart':
-  provider => shell,
-  command  => 'sudo service nginx restart',
-} 
