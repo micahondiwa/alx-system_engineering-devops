5cdd36d3faaa2a647a49fcb936899848f540663f
Micah Ondiwa <micah.oketch.ondiwa@gmail.com>
2023-09-13 13:12:30 +0300

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,3 +3,7 @@ exec { 'change-max-open-files':
   command => 'sudo sed -i "s/15/5000/" /etc/default/nginx; service nginx restart',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
