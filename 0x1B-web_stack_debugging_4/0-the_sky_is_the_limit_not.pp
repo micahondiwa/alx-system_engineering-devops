df9219ea840a1feb34dbec97d49bcca6ec3de50d
Micah Ondiwa <micah.oketch.ondiwa@gmail.com>
2023-10-10 11:23:54 +0300

    Delete 0x1B-web_stack_debugging_4 directory

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++
@@ -1,9 +0,0 @@
-# Fixing the problem of high amount of requests
-exec { 'change-max-open-files':
-  command => 'sudo sed -i "s/15/5000/" /etc/default/nginx; service nginx restart',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
-exec {'restart':
-  provider => shell,
-  command  => 'sudo service nginx restart',
-}
